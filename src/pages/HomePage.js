04de1deda482545d31a6b6b02eff9e468d3c431f
Robert J. Moss <robert.moss3409@gmail.com>
2022-04-22 15:51:15 -0400

    modified the Gallery elements on the HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,14 +6,22 @@ import { HeaderImages, HomePageGalleryImages } from '../images';
 
 const frontViewRendering = HeaderImages[0];
 
+const gallery1 = HomePageGalleryImages.slice(0, 2);
+const gallery2 = HomePageGalleryImages.slice(2, 4);
+
 const HomePage = () => {
+
+  const g = document.getElementById('hp-gallery-1');
+  console.log(g);
+
   return (
     <Layout>
       <HeaderImage
         imageSrc={frontViewRendering.src}
         imageAlt={frontViewRendering.alt}
       />
-      <Gallery id='hp-gallery-1' images={HomePageGalleryImages} />
+      <Gallery id='hp-gallery-1' images={gallery1} />
+      <Gallery id='hp-gallery-2' images={gallery2} />
       <Services />
     </Layout>
   );
