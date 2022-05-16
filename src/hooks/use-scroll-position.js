bfd51c61a4dffaae0284d47a350cf4464fcd67b4
Robert J. Moss <robert.moss3409@gmail.com>
2022-05-16 12:09:38 -0400

    refactor: makes useScrollPosition agnostic about device size
    Scroll and touchmove event listeners are added simulatenously instead of depending on the device size. This fixes the following issues: going from a small to large screen and not having features work because  a scroll listener wasn't added, and using a touch screen laptop but not being able to use touch functionality because a touchmove event listener wasn't added.

--- src/hooks/use-scroll-position.js
+++ src/hooks/use-scroll-position.js
@@ -1,22 +1,8 @@
 import { useState, useEffect } from 'react';
 
-const getDeviceSize = () => {
-  const viewportWidth = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  const viewportHeight = Math.max(
-    document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
-  );
-
-  return { viewportWidth, viewportHeight };
-};
-
 const getScreenPosition = () => {
   return { x: window.scrollX, y: window.scrollY };
 };
-
 
 const useScreenPosition = () => {
   const [screenPosition, setScreenPosition] = useState({
@@ -24,32 +10,24 @@ const useScreenPosition = () => {
     y: 0,
   });
 
-  let timer; 
-  
-  const viewportWidth = getDeviceSize().viewportWidth;
+  let timer;
 
   useEffect(() => {
-    
     const scrollHandler = () => {
       timer = setTimeout(() => {
         setScreenPosition(getScreenPosition());
       }, 300);
     };
 
-    if (viewportWidth < 500) {
-      window.addEventListener('touchmove', scrollHandler);
-    } else {
-      window.addEventListener('scroll', scrollHandler);
-    }
+    window.addEventListener('touchmove', scrollHandler);
+    window.addEventListener('scroll', scrollHandler);
+
     return () => {
       clearTimeout(timer);
-      if (viewportWidth < 500) {
-        window.removeEventListener('touchmove', scrollHandler);
-      } else {
-        window.removeEventListener('scroll', scrollHandler);
-      }
+      window.removeEventListener('touchmove', scrollHandler);
+      window.removeEventListener('scroll', scrollHandler);
     };
-  }, [viewportWidth]);
+  }, []);
 
   return screenPosition;
 };
