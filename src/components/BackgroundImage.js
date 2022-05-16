799c6429c8922769c08daa926c60fd09bd6403b7
Robert J. Moss <robert.moss3409@gmail.com>
2022-05-16 13:49:40 -0400

    refactor: makes BackgroundImage static

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -4,11 +4,12 @@ const BackgroundImage = (props) => {
   const background = {
     backgroundImage: `url(${props.src})`,
   };
+
   return (
     <div className={styles['background-container']}>
       <div
         style={background}
-        className={`${styles['background-image']} ${styles['span-animation']}`}
+        className={`${styles['background-image']}`}
       ></div>
     </div>
   );
