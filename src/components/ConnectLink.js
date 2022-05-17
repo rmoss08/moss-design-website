a091af5076f1499a4e71c88b56806df0fefecbcc
Robert J. Moss <robert.moss3409@gmail.com>
2022-05-17 11:12:28 -0400

    refactor: changed email address

--- src/components/ConnectLink.js
+++ src/components/ConnectLink.js
@@ -2,7 +2,7 @@ import styles from './ConnectLink.module.css';
 
 const ConnectLink = () => {
   return (
-    <a className={styles['connect-link']} href="mailto:robert.moss3409@gmail.com">
+    <a className={styles['connect-link']} href="mailto:john.smith@gmail.com">
       <h4>CONNECT</h4>
     </a>
   );
